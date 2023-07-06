cb92845c01e1d366b8ebd13b43788f95c1198ff0
Taichiro-S <hokkaido_mx_1_t.s@me.com>
2023-07-07 07:03:44 +0900

    supabase key added to env

--- src/hooks/useMutateUser.ts
+++ src/hooks/useMutateUser.ts
@@ -5,7 +5,7 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseAdmin = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL as string,
-  process.env.NEXT_PUBLIC_SERVICE_ROLE_KEY as string,
+  process.env.SUPABASE_SERVICE_ROLE_KEY as string,
 )
 
 type Passwords = {
